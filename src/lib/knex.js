fef8344ca3b0aa19f86473efc8c2e500bb77acd0
luizaugustogrein <luizaugustogrein@gmail.com>
2024-01-29 12:02:39 -0300

    bd

--- src/lib/knex.js
+++ src/lib/knex.js
@@ -1,11 +1,11 @@
 const knex = require('knex')({
     client: 'mysql2',
     connection: {
-        host: '185.211.7.52',
+        host: '',
         port: 3306,
-        user: 'u596350710_tiend_animal',
-        password: 'TiendAnimal!2023',
-        database: 'u596350710_tiend_animal'
+        user: '',
+        password: '',
+        database: ''
     }
 });
 
